ad26ac5cae53a2a3cf541d79cf54c1637b4c9335
luis rodriguez <luis.rodriguez@cru.org>
2014-08-15 16:01:41 -0400

    update

--- Berksfile
+++
@@ -1,5 +0,0 @@
-source "https://supermarket.getchef.com"
-
-metadata
-#cookbook "splunk_config", git: "git://github.com/cru-Luis-Rodriguez/splunk_config.git"
-#cookbook "chef-splunk"
--- metadata.rb
+++ metadata.rb
@@ -7,12 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 
-supports 'amazon'
-supports 'centos'
-supports 'debian'
-supports 'fedora'
-supports 'redhat'
-supports 'ubuntu'
-
-
 depends "chef-splunk"
